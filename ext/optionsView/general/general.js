f8743b508f3e0fc1c87145ead9817f31e308a4ba
jamesgu <jamesgu521@gmail.com>
2016-12-18 17:45:28 -0800

    Refine disable settings functionalities

--- ext/optionsView/general/general.js
+++ ext/optionsView/general/general.js
@@ -7,31 +7,33 @@ general.controller('generalController', function($scope, General) {
 
   // Watch for changes in model
   $scope.$watch('minutes', function(minutes) {
-    console.log('disable in controller called', minutes);
     if (minutes !== undefined) {
-      General.disable(minutes, getTimeLeft.bind(this));
+      General.disable(minutes);
     }
   })
 
   var getTimeLeft = function() {
-    console.log('getTimeLeft called');
+    console.log('getTImeleft called');
     chrome.extension.getBackgroundPage().chrome.alarms.getAll(function(alarms) {
       if (alarms.length > 0) {
         var d = new Date();
         var ms = d.getTime();
         
         var timeLeft = (alarms[0].scheduledTime - ms) / 60000; 
+        console.log(timeLeft, 'TIMELEFT');
         if (timeLeft > 60) {
           $scope.remainingTimeLeft.data = 'Disabled indefinitely'
         } else {
           timeLeft = parseFloat(Math.round(timeLeft * 100) / 100).toFixed(0); 
           $scope.remainingTimeLeft.data = `Fake news on the prowl for ${timeLeft} more minutes`;;
         }
+      } else {
+        $scope.remainingTimeLeft.data = 'Currently enabled';
       }
     }); 
   }
 
   getTimeLeft();
   // Poll for every half minute for live update on disabled time left
-  setInterval(function() { getTimeLeft(); $scope.$apply() }, 30000);
+  setInterval(function() { getTimeLeft(); $scope.$apply() }, 500);
 });
--- ext/optionsView/services.js
+++ ext/optionsView/services.js
@@ -13,14 +13,19 @@ services.factory('Themes', function() {
 services.factory('General', function() {
   
   // Disable for x minutes
-  var disable = function(min, callback) {
+  var disable = function(min, cb) {
 
     // If chose enabled, then change icon to enabled, set state to enabled and clear all if any alarms
     if (min === 0) {
       chrome.extension.getBackgroundPage().chrome.browserAction.setIcon({ path: "assets/turnip-white.png" },
         () => {
           chrome.extension.getBackgroundPage().setDisabledState(false, () => {
-          chrome.extension.getBackgroundPage().chrome.alarms.clearAll();
+          chrome.extension.getBackgroundPage().chrome.alarms.clear('disableAlarm', () => {
+            if (cb) {
+              console.log(cb, 'calling callback here')
+              cb();
+            }
+          });
         });      
       });
     } else {
@@ -30,7 +35,7 @@ services.factory('General', function() {
         () => {
           chrome.extension.getBackgroundPage().setDisabledState(true, () => {  
             // Set alarm at background level
-            chrome.extension.getBackgroundPage().setAlarm('disableAlarm', min);
+            chrome.extension.getBackgroundPage().setAlarm('disableAlarm', min, cb);
           });  
         });
     } 
