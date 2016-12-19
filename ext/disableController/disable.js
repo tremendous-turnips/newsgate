04b69d3e720254ab7206eff7597a63d08fc43dc8
jamesgu <jamesgu521@gmail.com>
2016-12-18 20:09:02 -0800

    Fix disableAlarm bugs due to integrating with updateBlacklist alarms

--- ext/disableController/disable.js
+++ ext/disableController/disable.js
@@ -12,7 +12,6 @@ var setAlarm = function(name, min, cb) {
 chrome.alarms.onAlarm.addListener(function alarmListener(alarm) {
   if (alarm.name === 'disableAlarm') {
     setDisabledState(false, function() {
-      console.log('Reverted back to enabled');
       chrome.extension.getBackgroundPage().chrome.browserAction.setIcon({ path: "assets/turnip-white.png" });
     });
   }
